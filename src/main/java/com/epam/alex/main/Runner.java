501d47ddd356d3f351a06ee1e5eb7a127ea27587
AlexandrSerebryakov <common190516@mail.ru>
2016-11-04 13:40:27 +0600

    Create entities, hierarchy and relations.

--- src/main/java/com/epam/alex/main/Runner.java
+++ src/main/java/com/epam/alex/main/Runner.java
@@ -1,7 +1,7 @@
 package com.epam.alex.main;
 
 /**
- * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
+ * Created by AlexandrSerebryakov on 03.11.2016.
  */
 public class Runner {
     public static void main(String[] args) {
---
+++ src/main/java/com/epam/alex/main/entity/TrainBooking.java
@@ -0,0 +1,7 @@
+package com.epam.alex.main.entity;
+
+/**
+ * Created by AlexandrSerebryakov on 04.11.2016.
+ */
+public class TrainBooking {
+}
