e4c47cb0663666c6f4f2469ca9932c404ffe1922
AlexandrSerebryakov <common190516@mail.ru>
2016-11-09 13:39:30 +0600

    Edit file pom.xml

--- src/main/java/com/epam/alex/entity/TravelDocument.java
+++ src/main/java/com/epam/alex/entity/TravelDocument.java
@@ -1,7 +1,15 @@
 package com.epam.alex.entity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 /**
  * Created by AlexandrSerebryakov on 04.11.2016.
  */
-public class TravelDocument extends Trip {
+public class TravelDocument  {
+    private static final Logger log = LoggerFactory.getLogger(TravelDocument.class);
+
+
 }
